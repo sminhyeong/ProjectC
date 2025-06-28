4cdbfe8f2aef02045e47794aaaf2f03f3bb94fbf
sminhyeong <96801506+sminhyeong@users.noreply.github.com>
2025-06-28 14:46:25 +0900

    2025.06.28 프로젝트에 C++클래스 추가하여 커밋
    2025.06.28
    UiViewManager.h
    UiViewManager.cpp
    추가하여 커밋

---
+++ Source/ProjectC/ProjectC.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectC.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectC, "ProjectC" );

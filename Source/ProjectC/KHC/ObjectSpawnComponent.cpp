a65b99f86e73c575af2e0e801761fd59835ac765
kwon-h-chan <a71179447@gmail.com>
2025-07-01 17:07:37 +0900

    아이템 스폰 컴포넌트 생성
    - 아이템 스폰 컴포넌트 생성
    - 기초 폴더 및 액터 생성

---
+++ Source/ProjectC/KHC/ObjectSpawnComponent.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ObjectSpawnComponent.h"
+
+// Sets default values for this component's properties
+UObjectSpawnComponent::UObjectSpawnComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UObjectSpawnComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (bAutoSpawn && IsValid(SpawnClass) && !HasSpawned)
+	{
+
+	}
+	
+}
+
+
+// Called every frame
+void UObjectSpawnComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...   
+}
+
---
+++ Source/ProjectC/KHC/ObjectSpawnComponent.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ObjectSpawnComponent.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PROJECTC_API UObjectSpawnComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UObjectSpawnComponent();
+	
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn")
+	bool bAutoSpawn = false;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn")
+	TSubclassOf<AActor> SpawnClass;
+
+	bool HasSpawned = false;
+};

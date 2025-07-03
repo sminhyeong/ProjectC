2f34cbdbb70120b83f17f87c8c332fa2ba8539e9
9dyy <79816837+9dyy@users.noreply.github.com>
2025-07-03 14:01:38 +0900

    250703 InventoryComponent -> InventoryManager로 대체
    - InventoryComponent 삭제
    + InventoryManager 추가

---
+++ Source/ProjectC/KDY/InventoryManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InventoryManager.h"
+
+// Sets default values
+AInventoryManager::AInventoryManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AInventoryManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AInventoryManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
